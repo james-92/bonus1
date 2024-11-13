b19ffe9e538e3024e2a96763d73614063b57b7cc
James.Lai <poyulai920908@gmail.com>
2024-11-13 10:24:26 +0800

    Initial Commit

---
+++ bonus1/bonus1App.swift
@@ -0,0 +1,17 @@
+//
+//  bonus1App.swift
+//  bonus1
+//
+//  Created by James.Lai on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct bonus1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bonus1Tests/bonus1Tests.swift
@@ -0,0 +1,16 @@
+//
+//  bonus1Tests.swift
+//  bonus1Tests
+//
+//  Created by James.Lai on 13/11/2024.
+//
+
+import Testing
+
+struct bonus1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
